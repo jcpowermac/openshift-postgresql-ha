557d8f0980034d3969d8c01ebdd9b41dd60c71f8
Joseph Callen <jcallen@redhat.com>
2017-04-19 16:38:30 -0400

    init script testing

---
+++ root/usr/bin/init.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import sys
+#import json
+#import logging
+import subprocess
+import socket
+#import time
+
+#logger = logging.getLogger(__name__)
+
+def main():
+    #logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', level=logging.INFO)
+    #logging.info("Changing the pod's role to %s" % sys.argv[2] )
+    print(sys.argv)
+#    patch = '{"metadata":{"labels":{"role": "%s"}}}' % sys.argv[2]
+
+#    returncode = subprocess.call(['/usr/bin/oc', 'patch','pod', socket.gethostname(), '-p', patch])
+#    print("return code %d" % returncode )
+
+if __name__ == "__main__":
+    main()
