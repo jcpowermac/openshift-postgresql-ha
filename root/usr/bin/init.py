9d808cf1d353eb4d9c0d0316f22c7c5c5a4d8561
Joseph Callen <jcallen@redhat.com>
2017-04-19 20:14:28 -0400

    adding some sleep

--- root/usr/bin/init.py
+++ root/usr/bin/init.py
@@ -6,7 +6,7 @@ import os
 #import logging
 import subprocess
 import socket
-#import time
+import time
 
 #logger = logging.getLogger(__name__)
 
@@ -14,6 +14,7 @@ def main():
     #logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', level=logging.INFO)
     #logging.info("Changing the pod's role to %s" % sys.argv[2] )
 
+    time.sleep(30)
     returncode = subprocess.call(['/opt/rh/rh-postgresql95/root/usr/bin/createdb',
         '-U', 'postgres', '-h', '127.0.0.1',
         '-O', os.environ.get('POSTGRESQL_USER'), os.environ.get('POSTGRESQL_DATABASE')])
