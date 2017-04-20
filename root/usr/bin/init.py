e86a2dd9281291ad9c58a8f45e9b472540c1f85b
Joseph Callen <jcallen@redhat.com>
2017-04-19 20:03:13 -0400

    oops

--- root/usr/bin/init.py
+++ root/usr/bin/init.py
@@ -13,11 +13,9 @@ import socket
 def main():
     #logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', level=logging.INFO)
     #logging.info("Changing the pod's role to %s" % sys.argv[2] )
-#    print(sys.argv)
-#    print(os.environ)
 
-    returncode = subprocess.call(['/opt/rh/rh-postgresql95/root/usr/bin/createdb', 
-        '-O', os.environ.get('POSTGRESQL_USER'), os.environ.get('POSTGRESQL_DATABASE'))
+    returncode = subprocess.call(['/opt/rh/rh-postgresql95/root/usr/bin/createdb', '-O',
+        os.environ.get('POSTGRESQL_USER'), os.environ.get('POSTGRESQL_DATABASE')])
 
     print("init.py createdb return code %d" % returncode )
 
